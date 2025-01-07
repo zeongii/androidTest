b252da064a41f4d0b6208000208fd3fd450f5020
zeongii <yaksanjy@naver.com>
2025-01-07 22:47:31 +0900

    oneFragment

--- ch11_jetpack/src/main/java/com/example/ch11_jetpack/MainActivity.kt
+++ ch11_jetpack/src/main/java/com/example/ch11_jetpack/MainActivity.kt
@@ -1,20 +1,71 @@
 package com.example.ch11_jetpack
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.ch11_jetpack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+
+    class MyFragmentPagerAdapter(activity: FragmentActivity): FragmentStateAdapter(activity){
+        val fragments: List<Fragment>
+        init {
+            fragments = listOf(OneFragment(), TwoFragment(), ThreeFragment())
+        }
+
+        override fun getItemCount(): Int = fragments.size
+        override fun createFragment(position: Int): Fragment = fragments[position]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.drawer_opened,
+            R.string.drawer_closed)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle.syncState()
+
+        val adapter = MyFragmentPagerAdapter(this)
+        binding.viewpager.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+
+        val menuItem = menu?.findItem(R.id.menu_search)
+        val searchView = menuItem?.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Log.d("kkang", "search text: $query")
+                return true
+            }
+        })
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
         }
+        return super.onOptionsItemSelected(item)
     }
 }
--- ch11_jetpack/src/main/java/com/example/ch11_jetpack/OneFragment.kt
+++ ch11_jetpack/src/main/java/com/example/ch11_jetpack/OneFragment.kt
@@ -1,30 +1,34 @@
 package com.example.ch11_jetpack
 
 import android.content.Context
-import android.content.res.Resources
+import android.graphics.BitmapFactory
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Rect
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.res.ResourcesCompat
-import androidx.fragment.app.Fragment
+import androidx.core.view.ViewCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ch11_jetpack.databinding.FragmentOneBinding
 import com.example.ch11_jetpack.databinding.ItemRecyclerviewBinding
 
 class MyViewHolder(val binding: ItemRecyclerviewBinding) : RecyclerView.ViewHolder(binding.root)
-class MyAdapter(val datas: MutableList<String>) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun getItemCount() : Int {
-        return datas.size
+
+class MyAdapter(val datas: MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    override fun getItemCount(): Int {
+        return datas.size;
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
-            RecyclerView.ViewHolder =
-        MyViewHolder(ItemRecyclerviewBinding.inflate(LayoutInflater.from(
-            parent.context),parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        MyViewHolder(
+            ItemRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent,
+                false))
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val binding = (holder as MyViewHolder).binding
@@ -40,27 +44,55 @@ class MyDecoration(val context: Context): RecyclerView.ItemDecoration() {
         val width = parent.width
         val height = parent.height
 
-        val dr: Drawable? = ResourcesCompat.getDrawable(context.getResources(), R.drawable.kbo, null)
-
-        val drWith = dr?.intrinsicWidth
+        val dr: Drawable? = ResourcesCompat.getDrawable(context.resources, R.drawable.kbo, null)
+        val drWidth = dr?.intrinsicWidth
         val drHeight = dr?.intrinsicHeight
-        val left = width / 2 - drWith?.div(2) as Int
-        val top = height / 2 - drHeight?.div(2) as Int
+
+        val left = width/2 - drWidth?.div(2) as Int
+        val top = height/2 - drHeight?.div(2) as Int
 
         c.drawBitmap(
-            BitmapFactiory.decodeResource(context.getResources(), R.drawable.kbo),
-            left.toFloat(),
-            top.toFloat(),
-            null
-        )
+            BitmapFactory.decodeResource(context.resources, R.drawable.kbo),
+            left.toFloat(), top.toFloat(), null)
+    }
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+        val index = parent.getChildAdapterPosition(view)+1
+        if(index % 3 == 0)
+            outRect.set(10, 10, 10, 60)
+        else
+            outRect.set(10, 10, 10, 0)
+
+        view.setBackgroundColor(Color.parseColor("#28A0FF"))
+        ViewCompat.setElevation(view, 20.0f)
     }
 }
-
 class OneFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return FragmentOneBinding.inflate(inflater, container, false).root
+        val binding = FragmentOneBinding.inflate(inflater, container, false)
+
+        val datas = mutableListOf<String>()
+        for(i in 1..10){
+            datas.add("Item $i")
+        }
+
+        val adapter = MyAdapter(datas)
+        val layoutManager = LinearLayoutManager(activity)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.addItemDecoration(MyDecoration(activity as Context))
+        return binding.root
+
     }
+
 }
